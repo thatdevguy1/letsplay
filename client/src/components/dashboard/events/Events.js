c0e911efdd16312efb74c12e3d9cc621e35d87c8
David Bland <davidbland@hotmail.com>
2021-01-03 16:51:09 -0500

    changed grid view to list view with icon placeholder for the type of event

--- client/src/components/dashboard/events/Events.js
+++ client/src/components/dashboard/events/Events.js
@@ -1,10 +1,21 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
 import { getEvents } from "../../../store/actions";
 import Event from "./event/Event";
+import List from "@material-ui/core/List";
 import "./Events.scss";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 930,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 function Events() {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const eventsInfo = useSelector((state) => state.eventsInfo);
 
@@ -14,22 +25,25 @@ function Events() {
 
   return (
     <div className="events">
-      {eventsInfo.toggleMyEvents === false
-        ? eventsInfo.events.map((event) => {
-            return event.name.toLowerCase().includes(eventsInfo.searchEvent) &&
-              event.public ? (
-              <Event key={event._id} eventData={event} />
-            ) : (
-              ""
-            );
-          })
-        : eventsInfo.myEvents.map((event) => {
-            return (
-              event.name.toLowerCase().includes(eventsInfo.searchEvent) && (
+      <List className={classes.root}>
+        {eventsInfo.toggleMyEvents === false
+          ? eventsInfo.events.map((event) => {
+              return event.name
+                .toLowerCase()
+                .includes(eventsInfo.searchEvent) && event.public ? (
                 <Event key={event._id} eventData={event} />
-              )
-            );
-          })}
+              ) : (
+                ""
+              );
+            })
+          : eventsInfo.myEvents.map((event) => {
+              return (
+                event.name.toLowerCase().includes(eventsInfo.searchEvent) && (
+                  <Event key={event._id} eventData={event} />
+                )
+              );
+            })}
+      </List>
     </div>
   );
 }
--- client/src/components/dashboard/events/event/Event.js
+++ client/src/components/dashboard/events/event/Event.js
@@ -1,27 +1,23 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
 import { useDispatch } from "react-redux";
 import { selectEvent } from "../../../../store/actions";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+// import Card from "@material-ui/core/Card";
+// import CardActionArea from "@material-ui/core/CardActionArea";
+// import CardActions from "@material-ui/core/CardActions";
+// import CardContent from "@material-ui/core/CardContent";
+// import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { Divider } from "@material-ui/core";
+// import Typography from "@material-ui/core/Typography";
+// import { Divider } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  root: {
-    width: "30vw",
-    minWidth: 245,
-    margin: 10,
-  },
-});
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ImageIcon from "@material-ui/icons/Image";
 
 function Event(props) {
-  const classes = useStyles();
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -31,40 +27,67 @@ function Event(props) {
   };
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="https://pbs.twimg.com/profile_images/531498936218189824/_8_2bNtP_400x400.jpeg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.eventData.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.eventData.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button
-          size="small"
-          color="primary"
-          onClick={(e) => {
-            e.preventDefault();
-            eventInfo(props.eventData);
-          }}
-        >
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    // <Card className={classes.root}>
+    //   <CardActionArea>
+    //     <CardMedia
+    //       component="img"
+    //       alt="Contemplative Reptile"
+    //       height="140"
+    //       image="https://pbs.twimg.com/profile_images/531498936218189824/_8_2bNtP_400x400.jpeg"
+    //       title="Contemplative Reptile"
+    //     />
+    //     <CardContent>
+    //       <Typography gutterBottom variant="h5" component="h2">
+    //         {props.eventData.name}
+    //       </Typography>
+    //       <Typography variant="body2" color="textSecondary" component="p">
+    //         {props.eventData.description}
+    //       </Typography>
+    //     </CardContent>
+    //   </CardActionArea>
+    //   <CardActions>
+    //     <Button size="small" color="primary">
+    //       Share
+    //     </Button>
+    // <Button
+    //   size="small"
+    //   color="primary"
+    //   onClick={(e) => {
+    //     e.preventDefault();
+    //     eventInfo(props.eventData);
+    //   }}
+    // >
+    //   Learn More
+    // </Button>
+    //   </CardActions>
+    // </Card>
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>
+          <ImageIcon />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={props.eventData.name}
+        secondary={props.eventData.description}
+        style={{
+          maxHeight: "66px",
+          overflow: "hidden",
+          width: "70%",
+          paddingRight: "20px",
+        }}
+      />
+      <Button
+        size="small"
+        color="primary"
+        onClick={(e) => {
+          e.preventDefault();
+          eventInfo(props.eventData);
+        }}
+      >
+        Learn More
+      </Button>
+    </ListItem>
   );
 }
 
