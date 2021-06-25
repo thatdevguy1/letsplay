acf2c485d8f2181de018f54ea35adc4cb62645b3
thatdevguy1 <dave@thatdevguy.me>
2021-06-24 21:45:35 -0400

    adjusted styling to gather information for the event into a box that hovers under the map and increased the width of the event list to 50%

--- client/src/components/dashboard/events/event-information/event-body/eventBody.js
+++ client/src/components/dashboard/events/event-information/event-body/eventBody.js
@@ -11,26 +11,51 @@ function EventBody(props) {
       Object.keys(props.eventInfo.selectedEvent).length > 0 ? (
         <div className="body-container">
           <h1>{props.eventInfo.selectedEvent.name}</h1>
-          <p className="description-wrapper">
-            {props.eventInfo.selectedEvent.description}
-          </p>
-          <div className="time-public-wrapper">
-            <div className="time-wrapper">
-              <span>Start time: {props.eventInfo.selectedEvent.startTime}</span>
-            </div>
-            <div className="public-wrapper">
-              <span>
-                This event is:
-                {props.eventInfo.selectedEvent.public ? (
-                  <span style={{ color: "blue" }}> Public</span>
-                ) : (
-                  <span style={{ color: "red" }}> Private</span>
-                )}
-              </span>
-            </div>
-          </div>
           <div className="map-wrapper">
             <Map type="displayLocation" />
+          </div>
+          <div className="event-details">
+            <p className="description-wrapper">
+              {props.eventInfo.selectedEvent.description}
+              {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+              vero omnis non dicta modi voluptate adipisci autem unde sit,
+              excepturi molestias voluptatem nesciunt repellendus, recusandae,
+              consequatur nisi facere itaque atque perferendis! Deserunt culpa
+              harum aliquid ab incidunt rem voluptate vero eaque voluptatem
+              porro natus dicta corporis vel earum odio molestias error
+              voluptatibus, eius qui? Similique blanditiis quae ipsam laboriosam
+              omnis dolores quod odit voluptatum tenetur porro. Maxime quo
+              expedita amet quaerat. Consequatur officia consectetur eius maxime
+              porro eum atque repudiandae, velit nihil harum tempora nesciunt
+              consequuntur quasi, voluptas animi delectus ratione sit et,
+              commodi perspiciatis. Iure vitae natus dolor, at est, reiciendis
+              totam iste earum sapiente itaque reprehenderit, iusto labore
+              perspiciatis pariatur? Deleniti incidunt aliquam eaque praesentium
+              corrupti esse rem consectetur, id tenetur, fuga veniam ipsam
+              accusantium consequuntur sunt aliquid voluptatibus! Exercitationem
+              nulla quas modi dolore veritatis possimus, perspiciatis rem! */}
+            </p>
+            <div className="time-public-wrapper">
+              <div className="date-wrapper">Date:</div>
+              <div className="time-wrapper">
+                Start time:
+                <span style={{ color: "white" }}>
+                  {" "}
+                  {props.eventInfo.selectedEvent.startTime}
+                </span>
+              </div>
+              <div className="public-wrapper">
+                <span>
+                  This is a
+                  {props.eventInfo.selectedEvent.public ? (
+                    <span style={{ color: "blue" }}> public </span>
+                  ) : (
+                    <span style={{ color: "red" }}> private </span>
+                  )}
+                  event
+                </span>
+              </div>
+            </div>
           </div>
           <div className="button-wrapper">
             {props.admin ? (
