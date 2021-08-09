9eb92e2ef19d71b9d6184b61bb775d253f058184
thatdevguy1 <dave@thatdevguy.me>
2021-08-09 17:17:09 -0400

    some development trickery

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,12 @@ import { Provider } from "react-redux";
 
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
+const store = createStore(
+const store = createStore(
+const store = createStore(
+const store = createStore(
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
