e791c10967f0a95de3ffd58cbc2c86a8d21350e9
David Bland <dave@thatdevguy.me>
2021-08-31 17:26:43 -0400

    nothing to report

--- client/src/index.js
+++ client/src/index.js
@@ -8,14 +8,14 @@ import thunk from "redux-thunk";
 import rootReducer from "./store/reducers";
 import { Provider } from "react-redux";
 
-// const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
